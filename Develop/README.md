58191445dd3ea62f2a51c4ea2175ae217a0da9bf
Tifni Shroll <tasshroll@gmail.com>
2023-04-19 18:51:50 -0700

    Links for GitHub license types added with badges

--- Develop/README.md
+++ Develop/README.md
@@ -223,3 +223,188 @@ N/A## Table of Contents
 ## Email
  sddfs
 
+# sldfj
+
+## Description
+ sldkfj
+
+## Table of Contents
+
+## Installation
+ sldkjf
+
+## Usage
+ lskdj
+
+## Tests
+ N/A
+
+## License
+ MIT License
+
+## GitHub
+ github.com/skdjh
+
+## Email
+ dh
+
+# holly
+
+## Description
+ sldkfafsdf saldkfjadfslk slldkjf
+
+[![License: Apache%202.0](https://img.shields.io/badge/License-Apache%202.0-yellow.svg)](https://opensource.org/licenses/Apache-2-0)
+
+## Table of Contents
+
+## Installation
+ sldkf
+
+## Tests
+ N/A
+
+## License
+ Apache License 2.0
+
+## GitHub
+ github.com/Not Defined
+
+## Email
+ Not Defined
+
+# tides
+
+## Description
+ swimming in ocean
+
+[![License: Apache%202.0](https://img.shields.io/badge/License-Apache%202.0-yellow.svg)](https://opensource.org/licenses/Apache-2-0)
+
+## Table of Contents
+
+## Tests
+ eat the sand
+
+## License
+ Apache License 2.0
+
+## GitHub
+ github.com/tasshroll
+
+## Email
+ Not Defined
+
+# labs
+
+## Description
+ dogs are best!
+
+[![License: MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Table of Contents
+
+## Contributors
+ me
+
+## License
+ MIT License
+
+# tennis ball
+
+## Description
+ sldkfjasdf
+
+[![License: MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Table of Contents
+
+## Installation
+ sdlkfjas
+
+## Tests
+ slkdf
+
+## License
+ MIT License
+
+## GitHub
+ github.com/sadf
+
+# sdfd
+
+## Description
+ sldkf
+
+[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)## Table of Contents
+
+## Installation
+ sddf
+
+## License
+ BSD 2-Clause Simplified License
+
+## GitHub
+ github.com/sddfd
+
+# fly
+
+## Description
+ Not Defined
+
+[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)
+
+## Table of Contents
+
+## Installation
+ asldkfj
+
+## License
+ Creative Commons Zero v1.0 Universal
+
+## GitHub
+ github.com/sadf
+
+# sdf
+
+## Description
+ sdfd
+
+[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)
+
+## Table of Contents
+
+## Installation
+ asddf
+
+## Usage
+ sdf
+
+## Contributors
+ sadfd
+
+## Tests
+ asdfd
+
+## License
+ BSD 2-Clause Simplified License
+
+# skdfj
+
+## Description
+ slkdf
+
+[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+
+## Table of Contents
+
+## Installation
+ sdfj
+
+## Tests
+ slkdf
+
+## License
+ Mozilla Public License 2.0
+
+## Questions
+ tasshroll
+ Contact me with additional questions at sldfk@fjf
--- Develop/index.js
+++ Develop/index.js
@@ -29,7 +29,7 @@ const questions = [
     {
         type: 'input',
         name: 'installation',
-        message: "List Installation Instructions. Use :wq (enter) when finished",
+        message: "List Installation Instructions",
         default() {
             return 'N/A';
         },
@@ -37,15 +37,7 @@ const questions = [
     {
         type: 'input', // switch back to 'editor',
         name: 'usage',
-        message: "List Usage Instructions. Type into the editor.\n To exit enter :wq (enter) when finished or :q!",
-        //         To issue commands in Vi/Vim, switch to command mode.
-
-        // 1. Press the Esc key.
-
-        // 2. You should see the ––INSERT–– label vanish from the lower-left.
-
-        // 3. To save your changes before you exit, type :w , and then Enter."
-
+        message: "List Usage Instructions.",
         default() {
             return 'N/A';
         },
@@ -70,8 +62,8 @@ const questions = [
     // THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
     ////////////////////////////
     {
-        type: "checkbox",
-        message: "Select one license type from choices",
+        type: "list",
+        message: "Select one license type below",
         name: "license",
         choices: [
             {
@@ -156,35 +148,74 @@ const questions = [
     // License, Contributing, Tests, and Questions
     // WHEN I enter my project title
 ]
-function getLicense(license) {
-
+function createBadge(license) {
     //[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
     // The badge URL format for Shields.io is https://img.shields.io/badge/<LABEL>-<MESSAGE>-<COLOR>.svg, 
     // <LABEL> is the label for the badge, 
     // <MESSAGE> is the message or text to be displayed, 
     // <COLOR> is the color of the badge.
     // Determine message
-    console.log("License is ", license[0]);
+    console.log("License is ", license);
     var message = ""
-    var color = "yellow.svg";
-    var licenseLine = "";
-    const url = `https://img.shields.io/badge/License`;
+    const url1 = `https://img.shields.io/badge/License`;
+    const url2 = `https://opensource.org/licenses/`;
 
-    // determine message
-    switch (license[0]) {
-        case "None":
-            message = "";
-            licenseLine = "N/A";
-            break;
+    //https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+
+    // get badge
+    // Source is from https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+    switch (license) {
         case "Apache License 2.0":
             message = "Apache%202.0"
-            licenseLine = `[![License: ${message}](${url}-${message}-${color})](https://opensource.org/licenses/Apache-2-0)\n\n`
+            return `[![License: ${message}](${url1}-${message}-blue.svg)](${url2}Apache-2-0)\n\n`
             //https://img.shields.io/badge/License-Apache%202.0-blue.svg
-            break;
+        case "GNU General Public License V3.0":
+            message = "GPLv3";
+            return `[![License: ${message}](${url1}-${message}-blue.svg)](${url2}GPL-3.0)\n\n`;
         case "MIT License":
             message = "MIT"
-            licenseLine = `[![License: ${license}](${url}-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n\n`
-            break;
+            return `[![License: ${license}](${url1}-${message}-yellow.svg)](${url2}MIT)\n\n`
+        case "BSD 2-Clause Simplified License":
+            message = "";
+            return `[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)\n\n`
+        // name: "GNU General Public License V3.0"
+        //     name: "BSD 2-Clause Simplified License"
+        case "BSD 3-Clause New or Revised License":
+            message = "";
+            return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)\n\n`
+        //     name: "BSD 3-Clause New or Revised License"
+        //     name: "Boost Software License"
+        case "Boost Software License":
+            message = "Boost_1.0";
+            return `[![License: ${license}](${url1}-${message}-yellow.svg)](https://www.boost.org/LICENSE_1_0.txt)\n\n`
+        //     name: "Creative Commons Zero v1.0 Universal"
+        case "Creative Commons Zero v1.0 Universal":
+            message = "";
+            return `[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)\n\n`;
+        //     name: "Eclipse Public License 2.0"
+        case "Eclipse Public License 2.0":
+            message = "";
+            return `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)\n\n`;
+        //     name: "GNU Affero General Public License v3.0"
+        case "GNU Affero General Public License v3.0":
+            message = "";
+            return `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)\n\n`;
+        //     name: "GNU General Public License v2.0"
+        case "GNU General Public License v2.0":
+            message = "";
+            return `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)\n\n`;
+        //     name: "GNU Lesser General Public License v2.1"
+        case "GNU Lesser General Public License v2.1":
+            message = "";
+            return `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)\n\n`;
+        //     name: "Mozilla Public License 2.0"
+        case "Mozilla Public License 2.0":
+            message = "";
+            return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)\n\n`;
+        //     name: "The Unlicense"
+        case "The Unlicense":
+            message = "";
+            return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)\n\n`;
         default:
             console.log("no license selected");
         // to encode URL, use %20 to replace a space and -- to replace a hyphen
@@ -227,7 +258,6 @@ function getLicense(license) {
 // };
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
     console.log(data);
     // destructure the object data
     const { title, description, installation, usage, contributors, tests, license, gitHub, email } = data;
@@ -238,28 +268,35 @@ function writeToFile(fileName, data) {
     // Try to use new fancy if then
     var installationLine = (installation != "N/A") ? `## Installation\n ${installation}\n\n` : "";
 
-    // Define the sections of the ReadMe
-    let titleLine = `# ${title}\n\n`;
-    let desciptionLine = `## Description\n ${description}\n\n`;
-    let tableOfContents = `## Table of Contents\n\n`;
-    var installationLine = (installation != "N/A") ? `## Installation\n ${installation}\n\n` : "";
+    // Assign the sections of ReadMe to the user input
+    // If user input is not entered, then omit that section from README
+    var titleLine = `# ${title}\n\n`;
+    var desciptionLine = `## Description\n ${description}\n\n`;
+    var tableOfContents = `## Table of Contents\n\n`;
+    var installationLine = (installation != "None") ? `## Installation\n ${installation}\n\n` : "";
     var usageLine = (usage != "N/A") ? `## Usage\n ${usage}\n\n` : "";
     var contributorLine = (contributors != "N/A") ? `## Contributors\n ${contributors}\n\n` : ""
-    let testsLine = `## Tests\n ${tests}\n\n`;
+    var testsLine = (tests != "N/A") ? `## Tests\n ${tests}\n\n` : "";
+      var questionsLine = (gitHub != "Not Defined" | email != "Not Defined") 
+    ? `## Questions\n GitHub: ${gitHub}\n Contact me with additional questions at ${email}`
+    : "";
     let licenseLine = `## License\n ${license}\n\n`;
-    let gitHubLine = `## GitHub\n github.com/${gitHub}\n\n`;
-    let emailLine = `## Email\n ${email}\n\n`;
-    var badgeLine = getLicense(license);
-    console.log("BadgeLine is", badgeLine);
+    var badgeLine = (license != "N/A") ? createBadge(license) : "";
     //[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
     var tableContents;
+    // - [Installation](#installation)
+    // - [Usage](#usage)
+    // - [Contributors](#Contributors)
+    // - [License](#license)
+    // - [GitHub]
+    // - [Email]
     // filter through data 
     // if a section is N/A, then exclude it from the table of contents
 
     // Order the sections of our README contents
-    let firstSection = titleLine + desciptionLine + badgeLine + tableOfContents + installationLine + usageLine + contributorLine + testsLine
-    let lastSection = licenseLine + gitHubLine + emailLine
+    let firstSection = titleLine + desciptionLine + badgeLine + tableOfContents + installationLine + usageLine
+    let lastSection = contributorLine + testsLine + licenseLine + questionsLine
     //console.log("Lines are are ", titleLine+desciptionLine+installationLine+usageLine+contributorLine);
 
     fs.appendFile(fileName, (firstSection + lastSection), (err) =>
@@ -357,4 +394,4 @@ function init() {
 init();
 // var license = "MIT"
 // var badge = "";
-// getLicense(license, badge);
+// createBadge(license, badge);
